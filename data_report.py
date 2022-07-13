a2b0612160eff8a5710cc730b82e77d8bef99199
Yunhao-Luo <86722488+Yunhao-Luo@users.noreply.github.com>
2022-07-13 12:22:01 -0700

    added read files and plot in timeline

--- data_report.py
+++ data_report.py
@@ -1,5 +1,8 @@
 import os
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
 
 class DataReport:
     def __init__(self, path):
@@ -26,14 +29,58 @@ class DataReport:
             for row in range(0, len(file)-1):
                 file[row]["Time"] = int(file[row+1].get("Elasped")) - int(file[row].get("Elasped"))
 
-
         for file in self.data:
-            for line in file:
+            for line in file[:]:
                 if (line["Source"] != "Marker") or "START" in line["Name"]:
                     file.remove(line)
+
+    def get_time(self):
+        time_data = []
+        for file in self.data:
+            time_data.append([])
+            time_content = time_data[-1]
+            for line in file:
+                time_content.append(int(line["Elasped"]) + int(line["Time"]))
+        return time_data
     
+    def get_name(self):
+        name_data = []
+        for file in self.data:
+            name_data.append([])
+            name_content = name_data[-1]
+            for line in file:
+                name = line["Name"].split(" ")[0]
+                name_content.append(name)
+        return name_data
+
     def plot_normal_line(self):
-        pass
+        time_data = self.get_time()
+        name_data = self.get_name()
+        fig, ax = plt.subplots(len(self.data),1)
+        levels = np.tile([-5, 5, -3, 3, -1, 1],
+                 int(np.ceil(len(time_data[0])/6)))[:len(time_data[0])]
+        for file_num in range(0,len(self.data)):
+            time_list = time_data[file_num]
+            name_list = name_data[file_num]
+            ax[file_num].vlines(time_list, 0, levels, color="tab:red")  # The vertical stems.
+            ax[file_num].plot(time_list, np.zeros_like(time_list), "-o",
+                    color="k", markerfacecolor="w")  # Baseline and markers on it.
+
+            # annotate lines
+            for d, l, r in zip(time_list, levels, name_data):
+                ax[file_num].annotate(r, xy=(d, l),
+                            xytext=(-3, np.sign(l)*3), textcoords="offset points",
+                            horizontalalignment="right",
+                            verticalalignment="bottom" if l > 0 else "top")
+
+            # remove y axis and spines
+            ax[file_num].yaxis.set_visible(False)
+            ax[file_num].xaxis.set_visible(False)
+            ax[file_num].spines[["left", "top", "right"]].set_visible(False)
+
+            ax[file_num].margins(y=0.1)
+
+        plt.show()
 
 
 
@@ -42,8 +89,19 @@ if __name__ == "__main__":
     test = DataReport(path)
     test.read_files()
     print("finish reading")
-    for file in test.data:
+    """  for file in test.data:
         print("new file")
         for line in file:
             print(line)
-            pass
+            pass """
+    res = test.get_time()
+    for i in res:
+        print(len(i))
+        print(i)
+
+    res = test.get_name()
+    for i in res:
+        print(len(i))
+        print(i)
+
+    test.plot_normal_line()
--- test2.py
+++ test2.py
@@ -1,62 +1,8 @@
-""" import matplotlib.pyplot as plt
-   
-Product = ['Computer','Monitor','Laptop','Printer','Tablet']
-Quantity = [320,450,300,120,280]
-
-ax = plt.barh(Product,Quantity)
-plt.title('Store Inventory')
-plt.ylabel('Product')
-plt.xlabel('Quantity')
-
-
-
-plt.show()
- """
-
-""" 
-import numpy as np
-import matplotlib.pyplot as plt
-
-#create data for two teams
-quarter = ['Q1', 'Q2', 'Q3', 'Q4']
-product_A = [14, 17, 12, 9]
-product_B = [7, 15, 24, 18]
-
-#define chart parameters
-N = 4 
-barWidth = 0.5
-xloc = np.arange(N)
-
-#create stacked bar chart
-p1 = plt.barh(xloc, product_A, height=barWidth, color='springgreen')
-p2 = plt.barh(xloc, product_B, height=barWidth, color='coral')
-
-#add labels, title, tick marks, and legend
-plt.title('Sales by Product & Quarter')
-
-#display chart
-plt.show() """
-
-""" import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-continent = ["Africa","Americas","Asia","Europe","Oceania"]
-lifeExp=[49,65,60,72,74]
-df = pd.DataFrame({"continent":continent, "lifeExp":lifeExp})
-
-plt.figure(figsize=(8, 6))
-splot=sns.barplot(x="continent",y="lifeExp",data=df)
-plt.xlabel("Continent", size=16)
-plt.ylabel("LifeExp", size=16)
-plt.bar_label(splot.containers[0],size=16,label_type='center')
-plt.show() """
-
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates as mdates
 
-dates = [0.1, 0.2, 0.53, 0.54, 0.75, 0.96, 1.1, 3.2, 3.53, 3.54, 4.75, 9.96]
+dates = [22, 26, 31, 35, 39, 43, 47, 51, 61, 65, 70, 79, 83, 88, 96, 101, 106, 110, 115, 119, 124, 132, 136, 140, 150, 155, 160, 171, 176, 182, 186, 190, 204, 209, 213, 217, 225, 229, 232, 235, 238, 248, 253, 257]
 names = ['a', 'b', 'c', 'd', 'e', 'f']
 
 # Choose some nice levels
@@ -84,7 +30,7 @@ ax[0].spines[["left", "top", "right"]].set_visible(False)
 
 ax[0].margins(y=0.1)
 
-dates = [3,4,6,8,8.5,9,11,12,13,0,0,0]
+dates = [22, 26, 31, 35, 39, 43, 47, 51, 61, 65, 70, 79, 83, 88, 96, 101, 106, 110, 115, 119, 124, 132, 136, 140, 150, 155, 160, 171, 176, 182, 186, 190, 204, 209, 213, 217, 225, 229, 232, 235, 238, 248, 253, 257]
 names = ['a', 'b', 'c', 'd', 'e', 'f']
 
 ax[1].vlines(dates, 0, levels, color="tab:red")  # The vertical stems.
